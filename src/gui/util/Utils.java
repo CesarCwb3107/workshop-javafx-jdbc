6b09487731e0d7f79dcf24b680ace17af2c8efde
Cesar F Silva <cesar.f.silva@gmail.com>
2020-05-19 18:08:21 -0300

    DepartmentForm (dialog) design

---
+++ src/gui/util/Utils.java
@@ -0,0 +1,14 @@
+package gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+	public static Stage currentStage(ActionEvent event) {
+		// Downcasting para Node.
+		// Window é uma super classe do Stage, tenho que fazer mais um
+		// Downcasting para stage.
+		return (Stage) ((Node)event.getSource()).getScene().getWindow();
+	}
+}
